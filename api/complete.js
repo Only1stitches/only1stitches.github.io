14b13e405727353c4418e0cd28f10179d5a50ea9
Only1stitches <uyistitches@gmail.com>
2026-03-22 11:20:34 +0100

    Update complete.js

--- api/complete.js
+++ api/complete.js
@@ -1,7 +1,6 @@
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { paymentId, txid } = req.body;
-    // This allows the Pi Browser to dismiss the "Pending Payment" popup
     return res.status(200).json({ message: "Success", paymentId, txid });
   }
   res.status(405).json({ message: "Method not allowed" });
